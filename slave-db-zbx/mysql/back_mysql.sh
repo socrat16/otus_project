60df71bb93467d2437adbdee34840d30afa0f553
nikita16 <71122445+socrat16@users.noreply.github.com>
2024-03-02 19:21:07 +0300

    Update and rename bachup.ssh to back_mysql.sh

---
+++ slave-db-zbx/mysql/back_mysql.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+# Путь для сохранения резервных копий
+BACKUP_DIR="/home/naagapov/mysql_bac"
+
+# Создание каталога для резервных копий с именем базы данных, если он не существует
+
+# Получение списка DB
+DB_NAMES=$(mysql  -e "show databases\g;" | awk '{print $1}' | grep -v '^Database')
+
+for DB_NAME in $DB_NAMES
+do
+    mkdir -p "${BACKUP_DIR}/${DB_NAME}"
+    TABLES=$(mysql -uroot $DB_NAME -e "SHOW TABLES;" | awk '{ print $1}' | grep -v '^Tables' )
+    for TABLE in $TABLES
+    do
+        mysqldump --skip-opt -uroot   $DB_NAME $TABLE > "${BACKUP_DIR}/${DB_NAME}/${TABLE}.sql" 
+    
+    done
+
+done
