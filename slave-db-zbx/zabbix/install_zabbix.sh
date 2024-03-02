b235dc497b568dc14da2bd406e47aa65d1164a5e
nikita16 <71122445+socrat16@users.noreply.github.com>
2024-03-02 14:17:39 +0300

    Update install_zabbix.sh

--- slave-db-zbx/zabbix/install_zabbix.sh
+++ slave-db-zbx/zabbix/install_zabbix.sh
@@ -1,4 +1,25 @@
+!#/bin/bash
 #Удаляем
 # sudo apt purge zabbix-server-mysql zabbix-frontend-php zabbix-agent
-#Устанвока
-https://www.zabbix.com/download?zabbix=6.4&os_distribution=ubuntu&os_version=22.04&components=server_frontend_agent&db=mysql&ws=apache
+#Установка
+#https://www.zabbix.com/download?zabbix=6.4&os_distribution=ubuntu&os_version=22.04&components=server_frontend_agent&db=mysql&ws=apache
+# wget https://repo.zabbix.com/zabbix/6.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.4-1+ubuntu22.04_all.deb
+# dpkg -i zabbix-release_6.4-1+ubuntu22.04_all.deb
+# apt update
+# apt install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent
+# mysql -uroot -p
+password
+mysql> create database zabbix character set utf8mb4 collate utf8mb4_bin;
+mysql> create user zabbix@localhost identified by 'password';
+mysql> grant all privileges on zabbix.* to zabbix@localhost;
+mysql> set global log_bin_trust_function_creators = 1;
+mysql> quit;
+# zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql --default-character-set=utf8mb4 -uzabbix -p zabbix
+# mysql -uroot -p
+password
+mysql> set global log_bin_trust_function_creators = 0;
+mysql> quit;
+# systemctl restart zabbix-server zabbix-agent apache2
+# systemctl enable zabbix-server zabbix-agent apache2
+
+# https://mykb.ru/topic/zbx_backup
