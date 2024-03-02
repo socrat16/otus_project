00234f289f6e95e2230c9c36d544f13d2453a8c0
nikita16 <71122445+socrat16@users.noreply.github.com>
2024-03-02 14:15:57 +0300

    Update install_zabbix.sh

--- slave-db-zbx/zabbix/install_zabbix.sh
+++ slave-db-zbx/zabbix/install_zabbix.sh
@@ -1,1 +1,4 @@
-
+#Удаляем
+# sudo apt purge zabbix-server-mysql zabbix-frontend-php zabbix-agent
+#Устанвока
+https://www.zabbix.com/download?zabbix=6.4&os_distribution=ubuntu&os_version=22.04&components=server_frontend_agent&db=mysql&ws=apache
