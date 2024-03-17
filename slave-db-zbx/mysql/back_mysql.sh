65111a570aeceefdeecb228f01ad86383c7f99a7
nikita16 <71122445+socrat16@users.noreply.github.com>
2024-03-17 14:23:48 +0300

    Update back_mysql.sh

--- slave-db-zbx/mysql/back_mysql.sh
+++ slave-db-zbx/mysql/back_mysql.sh
@@ -1,20 +1,18 @@
-#!/bin/bash
-# Путь для сохранения резервных копий
 BACKUP_DIR="/home/naagapov/mysql_bac"
-
-# Создание каталога для резервных копий с именем базы данных, если он не существует
 
 # Получение списка DB
 DB_NAMES=$(mysql  -e "show databases\g;" | awk '{print $1}' | grep -v '^Database')
 
 for DB_NAME in $DB_NAMES
 do
-    mkdir -p "${BACKUP_DIR}/${DB_NAME}"
-    TABLES=$(mysql -uroot $DB_NAME -e "SHOW TABLES;" | awk '{ print $1}' | grep -v '^Tables' )
-    for TABLE in $TABLES
-    do
-        mysqldump --skip-opt -uroot   $DB_NAME $TABLE > "${BACKUP_DIR}/${DB_NAME}/${TABLE}.sql" 
-    
-    done
-
+    if [ "$DB_NAME" = "wordpress" ]; then
+        mkdir -p "${BACKUP_DIR}/${DB_NAME}"
+        TABLES=$(mysql -uroot $DB_NAME -e "SHOW TABLES;" | awk '{ print $1}' | grep -v '^Tables' )
+        for TABLE in $TABLES
+        do
+#            if [ "$TABLE" = "wordpress" ]; then
+                mysqldump --create-options --set-gtid-purged=OFF  -uroot   $DB_NAME $TABLE > "${BACKUP_DIR}/${DB_NAME}/${TABLE}.sql"
+ #           fi
+        done
+    fi
 done
