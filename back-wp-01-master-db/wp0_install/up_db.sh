76910cbb40a1e45e3f194fbc3a274669c2a2f05f
nikita16 <71122445+socrat16@users.noreply.github.com>
2024-03-24 17:05:57 +0300

    Update up_db.sh

--- back-wp-01-master-db/wp0_install/up_db.sh
+++ back-wp-01-master-db/wp0_install/up_db.sh
@@ -1,7 +1,8 @@
+# Скрипт восстановления потаблично БД вордпресс из бекапа в папке mysql_bac
 echo "CREATE DATABASE wordpress;" | mysql -u root
 
 # Импортируем таблицы
-for table in /home/naagapov/back/db/wordpress/*.sql; do
+for table in /home/naagapov/wp0_install/mysql_bac/wordpress*.sql; do
 	    echo "SET FOREIGN_KEY_CHECKS=1;" > $table.tmp
 	        cat $table >> $table.tmp
 		    mysql -u root  wordpress < $table.tmp
