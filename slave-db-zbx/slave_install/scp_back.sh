715823181f54f92beacffaecadce88ecf7d57792
nikita16 <71122445+socrat16@users.noreply.github.com>
2024-03-24 17:46:50 +0300

    Delete slave-db-zbx/slave_install/scp_back.sh

--- slave-db-zbx/slave_install/scp_back.sh
+++
@@ -1,2 +0,0 @@
-scp -r  /home/naagapov/mysql_bac/* naagapov@10.129.0.21:/home/naagapov/back/db/
-# копирование бекапа на мастерscp -r  /home/naagapov/mysql_bac/* naagapov@10.129.0.21:/home/naagapov/back/db/
