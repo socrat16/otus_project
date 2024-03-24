99c6f93fe42278d7f1b84184965ae8f9cf7159f5
nikita16 <71122445+socrat16@users.noreply.github.com>
2024-03-24 20:19:10 +0300

    Update wp.sh

--- back-wp-02-elk/wp1_install/wp.sh
+++ back-wp-02-elk/wp1_install/wp.sh
@@ -1,10 +1,24 @@
 sudo apt update
-sudo apt install apache2 ghostscript libapache2-mod-php mysql-server php php-{bcmath,curl,imagick,intl,json,mysql,xml,zip} mbstring -y
+sudo apt install apache2 \
+                 ghostscript \
+                 libapache2-mod-php \
+                 mysql-server \
+                 php \
+                 php-bcmath \
+                 php-curl \
+                 php-imagick \
+                 php-intl \
+                 php-json \
+                 php-mbstring \
+                 php-mysql \
+                 php-xml \
+                 php-zip -y
+
 sudo mkdir -p /srv/www
 sudo chown www-data: /srv/www
 #sudo curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /srv/www
 sudo cp ./config/wordpress.conf /etc/apache2/sites-available/wordpress.conf
-sudo cp ./config/mysqld.conf    /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo cp ./config/mysqld.conf    /etc/mysql/mysql.conf.d/mysqld.conf
 sudo tar -xzf wordpress2.tar.gz  -P
 
 
